97a4ab0f9b6ea900f8e454cfcd45fd4bee229760
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-18 14:38:38 +0100

    implemented tracking current side-bar tab and tab content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import SideBar from '@/components/side-bar'
 import Main from '@/components/main'
 
 export default function Home() {
+  const [tab, setTab] = useState('pick-favourites')
+
   return (
     <Box display="flex" flexDirection="column" height="100vh">
       <Box component="header"></Box>
@@ -13,11 +15,11 @@ export default function Home() {
       {/* Main Content Wrapper */}
       <Box display="flex" flexGrow={1}>
         {/* Sidebar on the left */}
-        <SideBar />
+        <SideBar setTab={setTab} tab={tab} />
 
         {/* Main content on the right */}
         <Box component="section" flexGrow={1}>
-          <Main />
+          <Main tab={tab} />
         </Box>
       </Box>
     </Box>
