b7620b0f1709c7d834461971aa91e64823b81ed7
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-18 18:31:49 +0100

    nav bar is now fixed position

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -4,7 +4,16 @@ import { AppBar, Toolbar, Typography } from '@mui/material'
 export default function NavBar() {
   return (
     <div className="flex z-10">
-      <AppBar position="static" sx={{ mb: 3, background: '#333' }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          top: 0,
+          mb: 3,
+          background: '#322e2d',
+          height: '100px',
+          justifyContent: 'center',
+        }}
+      >
         <Toolbar>
           <Typography variant="h4" sx={{ flexGrow: 1 }}>
             CryptoWatcher
