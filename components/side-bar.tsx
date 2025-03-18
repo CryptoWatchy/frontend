01223041fbc322505147055979e654633cc4419c
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-03-18 14:39:15 +0100

    current tab tracking implemented

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import {
   Drawer,
@@ -7,7 +9,13 @@ import {
   ListItemText,
 } from '@mui/material'
 
-export default function SideBar() {
+export default function SideBar({
+  setTab,
+  tab,
+}: {
+  setTab: (tab: string) => void
+  tab: string
+}) {
   return (
     <Drawer
       variant="permanent"
@@ -16,20 +24,26 @@ export default function SideBar() {
         flexShrink: 0,
         '& .MuiDrawer-paper': {
           width: 240,
-          top: '64px', // Adjust this to match your NavBar's height
+          top: '64px',
           height: 'calc(100vh - 64px)', // Full height minus NavBar
         },
       }}
     >
       <List>
         <ListItem disablePadding>
-          <ListItemButton selected>
+          <ListItemButton
+            selected={tab === 'pick-favourites'}
+            onClick={() => setTab('pick-favourites')}
+          >
             <ListItemText primary="Pick favourites" />
           </ListItemButton>
         </ListItem>
 
         <ListItem disablePadding>
-          <ListItemButton>
+          <ListItemButton
+            selected={tab === 'my-cryptocurrencies'}
+            onClick={() => setTab('my-cryptocurrencies')}
+          >
             <ListItemText primary="My cryptocurrencies" />
           </ListItemButton>
         </ListItem>
